dffa0322aa847e1bc63baac8aff6c2ddeadf9df3
Osman Warsi <osman.warsi@live.com>
2020-10-25 00:22:22 -0400

    Made changes to ID TODO

--- unitTests.py
+++ unitTests.py
@@ -59,22 +59,22 @@ def test_POST_todo_without_title():
     
 #Osman code below
 
-#Tests for GET /todos/:id
-def test_GET_todo_id(id):
-    string = "http://localhost:4567/todos/" + str(id) 
-    response = requests.get(string)
+#Tests for GET /todos/:id VALID ID OF 1
+def test_GET_todo_id(): 
+    response = requests.get("http://localhost:4567/todos/1")
     response_body = response.json()
-    if (response.status_code == 200):
-        assert response.status_code == 200  
-        assert response_body["todos"][0]["id"] == id
-        #VALID ID  
-    elif (response.status_code == 404):
-        assert response.status_code == 404  
-        #INVALID ID  
+    assert response.status_code == 200  
+    assert response_body["todos"][0]["id"] == "1"
+
+#Tests for GET /todos/:id VALID ID OF 1
+def test_GET_todo_id_INVALID(): 
+    response = requests.get("http://localhost:4567/todos/69")
+    response_body = response.json()
+    assert response.status_code == 404  
 
     
 #Test for HEAD /todos/:id  with VALID id of 1
-def test_HEAD_todo(id):
+def test_HEAD_todo():
     response = requests.get("http://localhost:4567/todos/1")
     assert response.headers["Content-Type"] == "application/json"
     assert response.headers["Transfer-Encoding"] == "chunked"
