c93756df13df1123b4b626a3b3ffee5bd1fa30de
marw12 <marwan.khan@mail.mcgill.ca>
2020-10-24 21:33:22 -0400

    tests

--- unitTests.py
+++ unitTests.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-#check if http status code is 200
+#check if http status code is 200 when service is running
 def test_http_returns_code_200():
      response = requests.get("http://localhost:4567")
      assert response.status_code == 200
@@ -13,8 +13,6 @@ def test_GET_todo():
     response = requests.get("http://localhost:4567/todos")
     response_body = response.json()
     assert response.status_code == 200
-    assert response_body["todos"][0]["title"] == "file paperwork"
-    assert response_body["todos"][1]["title"] == "scan paperwork"
     
     
 #HEAD /todos  
@@ -31,7 +29,7 @@ def test_POST_todo_with_title():
             "title": "ecse 429",
             "description": "project"
             
-        }
+    }
     request_json = json.dumps(json_input)
     
     #create post request
@@ -43,7 +41,7 @@ def test_POST_todo_with_title():
     assert response_body["title"] == "ecse 429"
     
 
-#test POST /todos returns wrror code without title
+#test POST /todos returns wrror code without title (was not performed in exploratory testing)
 def test_POST_todo_without_title():
     json_input = {
             
@@ -56,6 +54,21 @@ def test_POST_todo_without_title():
     response = requests.post("http://localhost:4567/todos", request_json)
     assert response.status_code == 400
     
+    
+# GET /todos/:id
+def test_GET_todo_id():
+    response = requests.get("http://localhost:4567/todos/1")
+    response_body = response.json()
+    assert response.status_code == 200
+    assert response_body["todos"][0]["id"] == "1"
+    assert response_body["todos"][0]["title"] == "scan paperwork"
+
+
+#HEAD /todos/:id:
+def test_HEAD_todo_id():
+    response = requests.get("http://localhost:4567/todos/1")
+    assert response.headers["Content-Type"] == "application/json"
+    assert response.headers["Transfer-Encoding"] == "chunked"
     
     
     
