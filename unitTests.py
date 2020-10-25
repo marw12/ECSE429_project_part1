e8363cf11796f3f3fda581896c45e4b0e5e13b77
Osman Warsi <osman.warsi@live.com>
2020-10-24 21:32:24 -0400

    Added IDs tests

--- unitTests.py
+++ unitTests.py
@@ -57,7 +57,69 @@ def test_POST_todo_without_title():
     assert response.status_code == 400
     
     
+#Osman code below
+
+#Tests for GET /todos/:id
+def test_GET_todo_id(id):
+    string = "http://localhost:4567/todos/" + str(id) 
+    response = requests.get(string)
+    response_body = response.json()
+    if (response.status_code == 200):
+        assert response.status_code == 200  
+        assert response_body["todos"][0]["id"] == id
+        #VALID ID  
+    elif (response.status_code == 404):
+        assert response.status_code == 404  
+        #INVALID ID  
+
     
-    
-    
-    
+#Test for HEAD /todos/:id  with VALID id of 1
+def test_HEAD_todo(id):
+    response = requests.get("http://localhost:4567/todos/1")
+    assert response.headers["Content-Type"] == "application/json"
+    assert response.headers["Transfer-Encoding"] == "chunked"
+
+#test POST /todos/:id with title and description of VALID ID = 1
+def test_POST_todo_with_title():
+    json_input = {
+            
+            "title": "Testing Post with updated Title",
+            "description": "updated Description"
+            
+        }
+    request_json = json.dumps(json_input)
+    #create post request
+    response = requests.post("http://localhost:4567/todos/1", request_json)
+    assert response.status_code == 400
+
+#Test for DELETE /todos/:id with INVALID ID of 7
+def test_DELETE_invalidID():
+    response = requests.delete("http://localhost:4567/todos/7")
+    assert response.status_code == 404
+    #404 NOT FOUND ERROR EXPECTED HERE
+
+#Test for PUT /todos/:id with INVALID ID of 7
+def test_PUT_invalidID():
+    json_input = {
+            
+            "title": "Testing PUT with updated Title",
+            "description": "updated Description"
+            
+        }
+    request_json = json.dumps(json_input)
+    response = requests.put("http://localhost:4567/todos/7", request_json)
+    assert response.status_code == 404
+    #404 NOT FOUND ERROR EXPECTED HERE
+
+#Test for PUT /todos/:id with VALID ID of 1
+def test_PUT_invalidID():
+    json_input = {
+            
+            "title": "Testing PUT with updated Title",
+            "description": "updated Description"
+            
+        }
+    request_json = json.dumps(json_input)
+    response = requests.put("http://localhost:4567/todos/1",request_json)
+    assert response.status_code == 200
+  
